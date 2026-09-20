df8e7c97d18d3a9818c1c11a850d29dedc1693d5
Dedicatia <p314159265352022@outlook.com>
2026-09-20 14:00:29 +0800

    Update problem 17 and 18

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[allow(unused_imports)]
-use leetcodes::sol;
+use leetcodes::sol::four_sum;
 
 /// Only some important debug would use the main function for better print and lint. Otherwise, run tests in the #[cfg(test)] mod tests in `sol.rs`.
 fn main() {
-    println!("Hello, world!");
+    let v = vec![0,0,0,1000000000,1000000000,1000000000,1000000000];
+    let res = four_sum(v, 1000000000);
+    println!("Result: {:?}", res);
 }
--- src/sol.rs
+++ src/sol.rs
@@ -279,7 +279,7 @@ pub fn is_palindrome(x: i32) -> bool {
     }
 }
 
-/* 
+/*
 fn match_meta(ptn_char: char, a: char) -> bool {
     if ptn_char == '.' {
         true
@@ -474,7 +474,7 @@ pub fn longest_common_prefix(strs: Vec<String>) -> String {
 pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
     let mut nums = nums;
     nums.sort_unstable();
-    println!("After sort, the vector is {:?}", nums);
+
     let n = nums.len();
     let mut res = Vec::new();
     for i in 0..n - 1 {
@@ -496,8 +496,6 @@ pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
                 } else if nums[p1] + nums[p2] < target {
                     p1 += 1;
                 } else {
-                    println!("Push back: {}, {}, {}", nums[i], nums[p1], nums[p2]);
-                    println!("We're now at: {}, {}, {}", i, p1, p2);
                     res.push(vec![nums[i], nums[p1], nums[p2]]);
                     p2 -= 1;
                     p1 += 1;
@@ -514,8 +512,6 @@ pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
                         p1 += 1;
                     } else {
                         res.push(vec![nums[i], nums[p1], nums[p2]]);
-                        println!("Push back: {}, {}, {}", nums[i], nums[p1], nums[p2]);
-                        println!("We're now at: {}, {}, {}", i, p1, p2);
                         p2 -= 1;
                         p1 += 1;
                     }
@@ -540,11 +536,11 @@ pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
         let mut p1 = i + 1;
         let mut p2 = n - 1;
 
-        if nums[i] + nums[i+1] + nums[i+2] > closest && closest_diff > 0 {
+        if nums[i] + nums[i + 1] + nums[i + 2] > closest && closest_diff > 0 {
             break;
         }
 
-        if nums[i] + nums[p2] + nums[p2-1] < closest && closest_diff < 0 {
+        if nums[i] + nums[p2] + nums[p2 - 1] < closest && closest_diff < 0 {
             break;
         }
 
@@ -565,6 +561,186 @@ pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
         }
     }
     closest
+}
+
+/// This is the tool function used below (leetcode problem 17).
+/// It takes a string and a character (2-9) and returns a vector of strings with the character appended to the original string.
+fn string_push(original: &String, key: char) -> Vec<String> {
+    let value = match key {
+        '2' => "abc",
+        '3' => "def",
+        '4' => "ghi",
+        '5' => "jkl",
+        '6' => "mno",
+        '7' => "pqrs",
+        '8' => "tuv",
+        '9' => "wxyz",
+        _ => "",
+    };
+    let value_string = String::from(value);
+    let mut vect: Vec<String> = Vec::new();
+
+    for c in value_string.chars() {
+        let mut temp = original.clone();
+        temp.push(c);
+        vect.push(temp);
+    }
+    vect
+}
+
+/// This is the solution to the leetcode problem 17. letter combinations of a phone number.
+pub fn letter_combinations(digits: String) -> Vec<String> {
+    let mut res: Vec<String> = Vec::new();
+
+    for c in digits.chars() {
+        if res.is_empty() {
+            let mut temp = string_push(&String::from(""), c);
+            res.append(&mut temp);
+        } else {
+            let mut partial: Vec<String> = Vec::new();
+            for it in res.iter() {
+                let mut temp = string_push(it, c);
+                partial.append(&mut temp);
+            }
+            res = partial;
+        }
+    }
+    res
+}
+
+/// To handle overflow in the four_sum problem, we define an enum to represent the result of a safe sum operation.
+enum I32WithOverflow {
+    Item(i32),
+    Upward,
+    Downward,
+}
+
+fn safe_sum(numbers: &[i32]) -> I32WithOverflow {
+    let mut sum = 0i32;
+    for &num in numbers {
+        match sum.checked_add(num) {
+            Some(res) => sum = res,
+            None => {
+                if sum > 0 && num > 0 {
+                    return I32WithOverflow::Upward;
+                } else {
+                    return I32WithOverflow::Downward;
+                }
+            }
+        }
+    }
+    I32WithOverflow::Item(sum)
+}
+
+/// This is the leetcode solution to the problem 18."4Sum". That is, find all unique quadruplets in the array which gives the sum of the target. 
+/// Overflow might happen. Thus, we use a `safe_sum` function to handle the sum of integers (i32).
+pub fn four_sum(nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+    let mut nums = nums;
+    nums.sort_unstable();
+
+    let n = nums.len();
+    let mut res = Vec::new();
+
+    if n < 4 {
+        return res;
+    }
+
+    for i in 0..n - 3 {
+        match safe_sum(&nums[i..i + 4]) {
+            I32WithOverflow::Upward => {
+                break;
+            }
+            I32WithOverflow::Downward => {}
+            I32WithOverflow::Item(s) => {
+                if s > target {
+                    break;
+                }
+            }
+        }
+
+        match safe_sum(&[nums[i], nums[n - 3], nums[n - 2], nums[n - 1]]) {
+            I32WithOverflow::Downward => {
+                continue;
+            }
+            I32WithOverflow::Upward => {}
+            I32WithOverflow::Item(s) => {
+                if s < target {
+                    continue;
+                }
+            }
+        }
+
+        if i > 0 && nums[i] == nums[i - 1] {
+            continue;
+        }
+
+        for j in i + 1..n - 2 {
+            if j > i + 1 && nums[j] == nums[j - 1] {
+                continue;
+            }
+
+            match safe_sum(&[nums[i], nums[j], nums[j+1], nums[j+2]]) {
+                I32WithOverflow::Upward => {
+                    break;
+                }
+                I32WithOverflow::Downward => {}
+                I32WithOverflow::Item(s) => {
+                    if s > target {
+                        break;
+                    }
+                }
+            }
+
+            match safe_sum(&[nums[i], nums[j], nums[n - 2], nums[n - 1]]) {
+                I32WithOverflow::Downward => {
+                    continue;
+                }
+                I32WithOverflow::Upward => {}
+                I32WithOverflow::Item(s) => {
+                    if s < target {
+                        continue;
+                    }
+                }
+            }
+
+            let mut p1 = j + 1;
+            let mut p2 = n - 1;
+            let target1 = target - nums[i] - nums[j];
+
+            while p1 < p2 {
+                if p2 == n - 1 || p1 == j + 1 {
+                    if nums[p1] + nums[p2] > target1 {
+                        p2 -= 1;
+                        //println!("When p1 {}, p2 {}, no match, p2 <- 1.", p1, p2) // verbose debugging
+                    } else if nums[p1] + nums[p2] < target1 {
+                        p1 += 1;
+                        //println!("When p1 {}, p2 {}, no match, p1 -> 1.", p1, p2) // verbose debugging
+                    } else {
+                        res.push(vec![nums[i], nums[j], nums[p1], nums[p2]]);
+                        p2 -= 1;
+                        p1 += 1;
+                    }
+                } else {
+                    if nums[p1] == nums[p1 - 1] {
+                        p1 += 1;
+                    } else if nums[p2] == nums[p2 + 1] {
+                        p2 -= 1;
+                    } else {
+                        if nums[p1] + nums[p2] > target1 {
+                            p2 -= 1;
+                        } else if nums[p1] + nums[p2] < target1 {
+                            p1 += 1;
+                        } else {
+                            res.push(vec![nums[i], nums[j], nums[p1], nums[p2]]);
+                            p2 -= 1;
+                            p1 += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -597,7 +773,47 @@ mod test_modules {
 
     #[test]
     fn test_three_sum_closest_3() {
-        let v = vec![445,891,901,-869,-747,-750,112,411,-236,443,-759,-480,723,-702,624,-556,-763,-806,-682,-831,449,62,718,398,-937,-353,-968,497,504,-94,494,79,-26,430,-766,439,87,899,-216,18,-3,844,-160,351,646,-361,-375,-689,255,546,-846,294,-964,382,-998,-994,545,-162,-13,-519,-833,533,-975,974,920,-768,877,911,895,677,-625,-246,-355,639,499,-668,-117,807,662,-821,-264,525,428,-675,-917,-664,-933,-620,-844,784,-462,228,736,637,-424,395,345,-977,-980,-608,-258,-64,64,831,591,-780,-358,691,310,866,529,156,821,-461,369,832,-808,-16,92,-734,-485,776,192,-206,168,447,-54,-544,-192,864,-653,-868,97,337,-902,-108,-752,695,711,-802,-823,537,341,-477,-861,-169,77,-276,-815,-836,849,-524,-516,273,-273,423,-102,260,-213,-197,-932,81,-248,409,-694,681,900,-897,978,714,455,946,559,388,623,-839,-830,-935,-391,320,-552,-755,-458,-728,583,663,-131,-727,-372,576,476,947,220,-331,-124,39,-753,-963,284,76,-715,673,325,342,-152,-841,-79,495,5,389,-295,-925,181,-29,68,231,-574,-78,403,763,981,-148,-911,-711,998,-338,589,-565,-324,-673,-290,477,-825,472,431,-386,219,-247,-798,202,417,-698,164,396,851,-356,-527,-785,-126,-437,919,670,-644,-33,-811,602,-606,-116,-988,-837,13,956,780,674,798,435,441,264,709,953,232,-683,-120,318,-924,-985,-505,-232,-889,816,-564,-428,-427,-543,-10,625,454,-626,-58,486,-92,-618,-260,-443,577,527,949,245,-569,-285,-672,-573,-431,363,-733,-369,843,373,-572,721,-860,526,-680,479,618,598,887,-243,535,660,758,-71,266,-311,-528,283,-602,-24,114,-934,580,-435,-871,-993,-319,25,-274,859,-389,-96,375,692,-749,381,560,513,982,463,-592,336,-214,-460,374,850,-859,387,-879,205,-511,468,6,432,-268,10,103,-507,-226,-228,-326,-515,-422,-582,241,347,-414,-143,923,874,-89,297,-912,291,912,-65,161,-822,-784,-230,456,-553,556,-832,421];
+        let v = vec![
+            445, 891, 901, -869, -747, -750, 112, 411, -236, 443, -759, -480, 723, -702, 624, -556,
+            -763, -806, -682, -831, 449, 62, 718, 398, -937, -353, -968, 497, 504, -94, 494, 79,
+            -26, 430, -766, 439, 87, 899, -216, 18, -3, 844, -160, 351, 646, -361, -375, -689, 255,
+            546, -846, 294, -964, 382, -998, -994, 545, -162, -13, -519, -833, 533, -975, 974, 920,
+            -768, 877, 911, 895, 677, -625, -246, -355, 639, 499, -668, -117, 807, 662, -821, -264,
+            525, 428, -675, -917, -664, -933, -620, -844, 784, -462, 228, 736, 637, -424, 395, 345,
+            -977, -980, -608, -258, -64, 64, 831, 591, -780, -358, 691, 310, 866, 529, 156, 821,
+            -461, 369, 832, -808, -16, 92, -734, -485, 776, 192, -206, 168, 447, -54, -544, -192,
+            864, -653, -868, 97, 337, -902, -108, -752, 695, 711, -802, -823, 537, 341, -477, -861,
+            -169, 77, -276, -815, -836, 849, -524, -516, 273, -273, 423, -102, 260, -213, -197,
+            -932, 81, -248, 409, -694, 681, 900, -897, 978, 714, 455, 946, 559, 388, 623, -839,
+            -830, -935, -391, 320, -552, -755, -458, -728, 583, 663, -131, -727, -372, 576, 476,
+            947, 220, -331, -124, 39, -753, -963, 284, 76, -715, 673, 325, 342, -152, -841, -79,
+            495, 5, 389, -295, -925, 181, -29, 68, 231, -574, -78, 403, 763, 981, -148, -911, -711,
+            998, -338, 589, -565, -324, -673, -290, 477, -825, 472, 431, -386, 219, -247, -798,
+            202, 417, -698, 164, 396, 851, -356, -527, -785, -126, -437, 919, 670, -644, -33, -811,
+            602, -606, -116, -988, -837, 13, 956, 780, 674, 798, 435, 441, 264, 709, 953, 232,
+            -683, -120, 318, -924, -985, -505, -232, -889, 816, -564, -428, -427, -543, -10, 625,
+            454, -626, -58, 486, -92, -618, -260, -443, 577, 527, 949, 245, -569, -285, -672, -573,
+            -431, 363, -733, -369, 843, 373, -572, 721, -860, 526, -680, 479, 618, 598, 887, -243,
+            535, 660, 758, -71, 266, -311, -528, 283, -602, -24, 114, -934, 580, -435, -871, -993,
+            -319, 25, -274, 859, -389, -96, 375, 692, -749, 381, 560, 513, 982, 463, -592, 336,
+            -214, -460, 374, 850, -859, 387, -879, 205, -511, 468, 6, 432, -268, 10, 103, -507,
+            -226, -228, -326, -515, -422, -582, 241, 347, -414, -143, 923, 874, -89, 297, -912,
+            291, 912, -65, 161, -822, -784, -230, 456, -553, 556, -832, 421,
+        ];
         assert_eq!(three_sum_closest(v, -7580), -2985)
+    }
+
+    #[test]
+    fn test_four_sum() {
+        let v = vec![0, 0, 0, 0];
+        let res = vec![vec![0, 0, 0, 0]];
+        assert_eq!(four_sum(v, 0), res)
+    }
+
+    #[test]
+    fn test_four_sum_overflow() {
+        let v = vec![0,0,0,1000000000,1000000000,1000000000,1000000000];
+        let expected = vec![vec![0,0,0,1000000000]];
+        assert_eq!(four_sum(v, 1000000000), expected)
     }
 }
